00ecc27f5deeaefd328b94f9a904cca2a4759304
Dmitry Savitski <dmitry.savitsky@rightscale.com>
2014-03-03 18:03:25 +0300

    Make solutions to problems separate files

--- problems/0002_Even_Fibonacci_numbers/solution.rb
+++
@@ -1,5 +0,0 @@
-require File.join(File.expand_path(File.dirname(__FILE__)), '../..' , 'generators', 'fibonacci_generator')
-
-fib = FibonacciGenerator.new
-puts fib.take_while {|s| s < 4000000 }.select {|s| s.even? }.reduce(:+)
-
